41e66f78322e3776e5b7382a44d285b5ec60140d
fedrickfairerman <fedrick.fairerman@gmail.com>
2026-05-13 20:28:58 -0300

    codigo refeito

--- npc.py
+++ npc.py
@@ -5,12 +5,67 @@ absorb = []
 vul_num = 0
 cond_imu = []
 
+
+def escolher_opcao():
+    while True:
+        try:
+            opcao_escolhida = int(input('Digite o numero da opção '))
+            if opcao_escolhida in [1,2,3]:
+                return opcao_escolhida
+                
+            else:
+                print('Opcão invalida!')
+        except ValueError:
+            print('Opção invalida! Digite apenas numeros')
+
+
 def exibir_opcao():
     print('1 - Soldado - NPC basico')
     print('2 - Elite - Um NPC mais perigoso')
     print('3 - Campeão - Um NPC Chefe, normalmente o Boss de alguma batalha epica!')
 
-def tipo_criatura(nivel,wlp,dex,ins,vida,mp,defesa,defesamagica,classe,mult=1,):
+def intro():
+    print('Bem vindo ao criador de NPC de Fabula Ultima')
+    print('Que tipo de NPC você irá criar?')
+    exibir_opcao()
+    tipo = escolher_opcao()
+    if tipo == 1:
+        print('Iremos criar um soldado')
+    elif tipo == 2:
+        print('Iremos criar um Elite')
+    elif tipo == 3:
+        print('Iremos criar um Campeão')
+    
+    nivel = int(input('Informe o nivel do personagem '))
+    wlp = int(input('Informe o dado de Willpower, diga 6, 8 ou 10 '))
+    dex = int(input('Informe o dado de Dexterity, diga 6, 8 ou 10 '))
+    ins = int(input('Informe o dado de Insight, diga 6, 8 ou 10 '))
+    mig = int(input('Informe o dado de Might, diga 6, 8 ou 10 '))
+    mp = (nivel + (ins * 5))
+    init = 0
+    skill_number = 0
+    if tipo ==3:   
+        mult = int(input('Quantos Soldados esse Campeão irá substituir? '))
+    
+    if tipo == 1:
+        estilo = 'Soldado'
+        vida = (nivel * 2) + (mig * 5)
+    elif tipo == 2:
+        estilo = 'Elite'
+        vida = ((nivel * 2) + (mig * 5)) * 2
+        init += 2
+        skill_number = skill_number + 1
+    elif tipo == 3:
+         estilo = 'Campeão'
+         vida = ((nivel * 2) + (mig * 5)) * mult
+         mp = mp * 2
+         init = init + mult
+         skill_number = skill_number + mult
+    crise = vida // 2
+    skill_number += (nivel // 10)
+    defesa = dex
+    defesamagica = wlp
+    iniciativa_final = ((dex + ins) // 2) + init
     print('Escolha qual tipo a criatura irá ser: ')
     print('1 - Humanoide, ganha a Skill Equipment de graça')      
     print('2 - Monstro')
@@ -21,8 +76,7 @@ def tipo_criatura(nivel,wlp,dex,ins,vida,mp,defesa,defesamagica,classe,mult=1,):
     print('7 - Planta, imune as condições Dazed, Shaken e Enraged, vulneravel a um dano a escolha sua')
     print('8 - Undead, imune a Dark e Poison, a Poisoned e vulneravel a Light')
     criatura = 'nada'
-    skill_num = 0
-    tipo_escolhido = int(input('Digite o numero 1-8'))
+    tipo_escolhido = int(input('Digite o numero 1-8 '))
     if tipo_escolhido == 1:
             criatura = 'Humanoide'
     elif tipo_escolhido == 2:
@@ -39,7 +93,7 @@ def tipo_criatura(nivel,wlp,dex,ins,vida,mp,defesa,defesamagica,classe,mult=1,):
             resist.extend([demon_new_resist_1, demon_new_resist_2])
     elif tipo_escolhido == 5:
             criatura = 'Fera'
-            skill_num += 4
+            skill_number += 4
     elif tipo_escolhido == 6:
             criatura = 'Elemental'
             elemental_new_imu = input('Escolha um novo tipo de dano que ele será Resistente: Fire, Earth, Ice, Air, Physical, Light, Dark, Bolt: ')
@@ -54,82 +108,21 @@ def tipo_criatura(nivel,wlp,dex,ins,vida,mp,defesa,defesamagica,classe,mult=1,):
             criatura = 'Undead'
             imu.extend(['Dark','Poison'])
             vul.append('Light')
-    
 
 
-def criar_elite():
-    nivel = int(input('Informe o nivel do personagem '))
-    wlp = int(input('Informe o dado de Willpower, diga 6, 8 ou 10 '))
-    dex = int(input('Informe o dado de Dexterity, diga 6, 8 ou 10 '))
-    ins = int(input('Informe o dado de Insight, diga 6, 8 ou 10'))
-    mig = int(input('Informe o dado de Might, diga 6, 8 ou 10 '))
-    vida = ((nivel * 2) + (mig * 5)) * 2
-    mp = nivel + (ins * 5)
-    defesa = dex
-    defesamagica = wlp
-    classe = 'elite'
-    tipo_criatura(nivel,wlp,dex,ins,vida,mp,defesa,defesamagica,classe)
-    
-
-def criar_campeao():
-    nivel = int(input('Informe o nivel do personagem '))
-    wlp = int(input('Informe o dado de Willpower, diga 6, 8 ou 10 '))
-    dex = int(input('Informe o dado de Dexterity, diga 6, 8 ou 10 '))
-    ins = int(input('Informe o dado de Insight, diga 6, 8 ou 10'))
-    mig = int(input('Informe o dado de Might, diga 6, 8 ou 10 '))
-    mult = int(input('Quantos Soldados esse Campeão irá substituir? '))
-    vida = ((nivel * 2) + (mig * 5)) * mult
-    mp = (nivel + (ins * 5)) * 2
-    defesa = dex
-    defesamagica = wlp
-    classe = 'campeão'
-    tipo_criatura(nivel,wlp,dex,ins,vida,mp,defesa,defesamagica,classe,mult)
-
-def criar_soldado():
-    nivel = int(input('Informe o nivel do personagem '))
-    wlp = int(input('Informe o dado de Willpower, diga 6, 8 ou 10 '))
-    dex = int(input('Informe o dado de Dexterity, diga 6, 8 ou 10 '))
-    ins = int(input('Informe o dado de Insight, diga 6, 8 ou 10'))
-    mig = int(input('Informe o dado de Might, diga 6, 8 ou 10 '))
-    vida = (nivel * 2) + (mig * 5)
-    mp = nivel + (ins * 5)
-    print(vida)
-    print(mp)
-    defesa = dex
-    defesamagica = wlp
-    classe = 'Soldado'
-    tipo_criatura(nivel,wlp,dex,ins,vida,mp,defesa,defesamagica,classe)
-    
-
-
-def escolher_opcao():
-    while True:
-        try:
-            opcao_escolhida = int(input('Digite o numero da opção '))
-            if opcao_escolhida == 1:
-                print('Vamos criar um Soldado!')
-                criar_soldado()
-            elif opcao_escolhida == 2:
-                print('Vamos criar um Elite!')
-                criar_elite()
-                
-            elif opcao_escolhida == 3:
-                print('Vamos criar um Campeão!')
-                criar_campeao()
-                
-            else:
-                print('Opcão invalida!')
-        except ValueError:
-            print('Opção invalida! Digite apenas numeros')
-            
-
-
-def intro():
-    print('Bem vindo ao criador de NPC de Fabula Ultima')
-    print('Que tipo de NPC você irá criar?')
-    exibir_opcao()
-    escolher_opcao()
-
+    print("\n--- STATUS GERADOS ---")
+    print(f'Você criou um {criatura} {estilo}' )
+    print(f"Vida: {vida} Crise: {crise}| MP: {mp}")
+    print(f"Defesa: {defesa} | Defesa Mágica: {defesamagica}")
+    print(f"Iniciativa: {iniciativa_final}")
+    print(f"Número de Skills: {skill_number}")
+    print(f"Iniciativa: {iniciativa_final}")
+    print("-" * 30)
+    print(f"Imunidades: {', '.join(imu) if imu else 'Nenhuma'}")
+    print(f"Resistências: {', '.join(resist) if resist else 'Nenhuma'}")
+    print(f"Vulnerabilidades: {', '.join(vul) if vul else 'Nenhuma'}")
+    print(f"Imunidade a Condições: {', '.join(cond_imu) if cond_imu else 'Nenhuma'}")
+    print("=" * 30)
 
 if __name__ == '__main__':
     intro()
